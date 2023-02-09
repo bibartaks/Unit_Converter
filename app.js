269e62f81c4fca8944f4d7a4b90771092f8621c6
Bibarta Kumar Sarkar <106395132+bibartaks@users.noreply.github.com>
2023-02-09 10:12:05 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const convertBtn = document.getElementById("convert-btn")
+let inputEl = document.getElementById("input")
+let lengthEl = document.getElementById("leangth-el")
+let volumeEl = document.getElementById("volume-el")
+let massEl = document.getElementById("mass-el")
+
+
+const meterToFeet =  3.281
+const literToGallon =  0.264
+const kiloToPound =  2.204
+
+convertBtn.addEventListener("click", function() {
+    const baseValue = inputEl.value
+
+    const metter = Number(baseValue * meterToFeet).toFixed(3)
+    const feet  = Number(baseValue / meterToFeet).toFixed(3)
+    lengthEl.textContent = `${baseValue} meter = ${metter} feet | ${baseValue} feet = ${feet} meters`
+
+    const liters = Number(baseValue * literToGallon).toFixed(3)
+    const gallons = Number(baseValue / literToGallon).toFixed(3)
+    volumeEl.textContent = `${baseValue} liters = ${liters} gallons | ${baseValue} gallons = ${gallons} liters` 
+
+    const kilos = Number(baseValue * kiloToPound).toFixed(3)
+    const pounds = Number(baseValue / kiloToPound).toFixed(3)
+    massEl.textContent = `${baseValue} kilos = ${kilos} | ${baseValue} pounds = ${pounds} kilos`
+})
